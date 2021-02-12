518212d59290b680deeeddb4e3ef22c1aa6945d3
Gerard Hynes <gkhynes@gmail.com>
2021-02-12 21:05:57 +0000

    Clean up styles and comments

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A header parser microservice built for the freeCodeCamp APIs and Microservices C
 
 [View live project](https://request-header-parser-microservice.gkhynes.repl.co)
 
+![Screenshot of Request Header Parser Microservice](https://res.cloudinary.com/gerhynes/image/upload/q_auto/f_auto/v1613163810/Screenshot_2021-02-12_Request_Header_Parser_Microservice_zm05tk.png)
+
 ### Requirements
 
 - A request to `/api/whoami` should return a JSON object with your IP address in the `ipaddress` key.
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// enable CORS so the API is remotely testable by FCC
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -24,7 +21,6 @@ app.get("/api/whoami", (req, res) => {
   res.json({ ipaddress, language, software });
 });
 
-// listen for requests :)
 app.listen(port, function () {
   console.log(`Your app is listening on port ${port}`);
 });
